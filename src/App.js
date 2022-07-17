d586c521ec39d759ca2812e82097a26ed8de6c8c
Saurabh Tomar <saurabhtomar116@gmail.com>
2022-07-17 14:15:52 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -2,33 +2,17 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import Texttab from './Texttab';
 import './App.css';
-//import { SpectrumVisualizer, SpectrumVisualizerTheme } from 'react-audio-visualizers';
-//import { AudioVisualizer, AudioVisualizerCommonProps } from 'react-audio-visualizers-core';
-import Video from "./video.mp4";
+
 import audio from "./audio.mp3";
-//import  {VideoDuration}  from './VideoDuration';
 import{ useEffect, useState } from "react";
-//import Visualizer from './visualizer';
 import { WaveformVisualizer, WaveformVisualizerTheme } from 'react-audio-visualizers';
 
 
 function App() {
-//  let media = new Audio({Video});
-  //media.onloadedmetadata = function() {
- // media.duration; // this would give duration of the video/audio file
-  //console.log(media.duration);
-  //};
-//alert(Video.duration);
+
 const [visible, setVisible] = useState(true);
 const [play,setplay]=useState();
-/*useEffect(() => {
-  setTimeout(() => {
-    setVisible(false);
-  }, props.delay);
-}, [props.delay]);
 
-return visible ? <Texttab/> : <div />;
-*/
 
 const deleteTab =(data)=>{
   
@@ -63,5 +47,3 @@ const addTab= ()=>{
   );
 }
 export default App;
-//<video controls autostart autoPlay src={Video} type="video/mp4" />
-//<audio controls autostart autoPlay src ={audio} />
